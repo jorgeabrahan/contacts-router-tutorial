aa26b8d5f8dbdb6813b1466dcbc3f65325208386
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-21 00:30:28 -0600

    Create project setup files

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
